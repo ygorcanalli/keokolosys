c0156ed21fcc9fe377e2d5814ad50d96109248f4
LuizFernando-RO <luizoliveira.lfro@gmail.com>
2014-07-08 13:16:19 -0300

    funcionalidade de incluir instituicao

--- view/administrativo/SwingCadastrarInstituicao.java
+++ view/administrativo/SwingCadastrarInstituicao.java
@@ -1,8 +1,5 @@
 package administrativo;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -15,42 +12,110 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
-import administrativo.ControleCadastrarInstituicao;
+import excecao.ExcecaoDeCadastro;
 
-public class SwingCadastrarInstituicao extends JFrame {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Collection;
 
-	/**
-	 * 
-	 */
+public class SwingCadastrarInstituicao extends JFrame implements AbstractGUICadastrarInstituicao{
+
 	private static final long serialVersionUID = 7379748951410562202L;
 	private JPanel contentPane;
 	
 	private ControleCadastrarInstituicao controleCadastrarInstituicao;
+	
+	private JComboBox<String> comboBoxInstituicao;
+	
 	private JTextField textFieldNomeInstituicao;
 	private JTextField textFieldLocalizacao;
 	private JTextField textFieldSilga;
+	
+	private JButton btnNova;
+	private JButton btnSalvar;
+	private JButton btnEditar;
+	private JButton btnExcluir;
+	private JButton btnSair;
+	
+	private boolean activeAction;
+	
+	private char activeMnemonic;
+	
+	@Override
+	public void tonarVisivel(){
+		
+		this.setVisible(true);
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SwingCadastrarInstituicao frame = new SwingCadastrarInstituicao();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		this.comboBoxInstituicao.setEnabled(false);
+		
+		this.textFieldNomeInstituicao.setEnabled(false);
+		this.textFieldLocalizacao.setEnabled(false);
+		this.textFieldSilga.setEnabled(false);
+		
+		this.btnSalvar.setEnabled(false);
+	}
+	
+	@Override
+	public void fechar(){
+		
+		this.setVisible(false);
+	}
+	
+	public SwingCadastrarInstituicao(ControleCadastrarInstituicao controleCadastrarInstituicao){
+		
+		this.controleCadastrarInstituicao = controleCadastrarInstituicao;
+		inicializarFrame();
+		carregarInstituicoes();
+	}
+	
+	@Override
+	public void inicializar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exibirMensagemDeErro(String mensagem, String titulo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exibirMensagemDeAviso(String mensagem, String titulo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exibirMensagemDeInformacao(String mensagem, String titulo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Integer exibirMensagemDeConfirmacao(String mensagem, String titulo,
+			Object[] opcoes, Object opcaoPadrao) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void carregarInstituicoes() {
+		
+		Collection<String> instituicoesStr = controleCadastrarInstituicao.carregarInstituicoes(); 
+		
+		for (String instituicao : instituicoesStr) {
+			
+			this.comboBoxInstituicao.addItem(instituicao);
+		}
+		
 	}
 
 	/**
 	 * Create the frame.
 	 */
-	public SwingCadastrarInstituicao() {
-		setTitle("Cadastrar institui\u00E7\u00E3o de ensino ou pesquisa");
+	private void inicializarFrame() {
+		setTitle("Cadastrar instituicao de ensino ou pesquisa");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 539, 365);
 		contentPane = new JPanel();
@@ -86,34 +151,161 @@ public class SwingCadastrarInstituicao extends JFrame {
 					.addContainerGap(37, Short.MAX_VALUE))
 		);
 		
-		JButton btnNova = new JButton("Nova");
-		btnNova.setMnemonic('N');
+		this.btnNova = new JButton("Nova");
+		btnNova.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				activeAction = true;
+				
+				textFieldNomeInstituicao.setEnabled(true);
+				textFieldLocalizacao.setEnabled(true);
+				textFieldSilga.setEnabled(true);
+				
+				btnEditar.setEnabled(false);
+				btnExcluir.setEnabled(false);
+				btnNova.setEnabled(false);
+				
+				btnSalvar.setEnabled(true);
+				
+				activeMnemonic = 'N';
+			}
+		});
+		this.btnNova.setMnemonic('N');
 		
-		JButton btnSalvar = new JButton("Salvar");
-		btnSalvar.setMnemonic('r');
+		this.btnSalvar = new JButton("Salvar");
+		btnSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				//acao salvar
+				
+				switch(activeMnemonic){
+				
+				case 'N':
+				{			
+					try {
+												
+						controleCadastrarInstituicao.cadastrarInstituicao(textFieldNomeInstituicao.getText(), textFieldSilga.getText(), textFieldLocalizacao.getText());
+					
+					} catch (ExcecaoDeCadastro e) {
+						
+						e.printStackTrace();
+					}
+					
+					comboBoxInstituicao.removeAllItems();
+					carregarInstituicoes();
+				}
 		
-		JButton btnEditar = new JButton("Editar");
-		btnEditar.setMnemonic('E');
+					break;
+					
+				case 'E':
+					
+				case 'x':
+				}
+				
+				textFieldNomeInstituicao.setText("");
+				textFieldSilga.setText("");
+				textFieldLocalizacao.setText("");
+				
+				textFieldNomeInstituicao.setEnabled(false);
+				textFieldSilga.setEnabled(false);
+				textFieldLocalizacao.setEnabled(false);
+				
+				btnNova.setEnabled(true);
+				btnEditar.setEnabled(true);
+				btnExcluir.setEnabled(true);
+				
+				btnSalvar.setEnabled(false);
+				
+				activeAction = false;
+			}
+		});
+		this.btnSalvar.setMnemonic('r');
 		
-		JButton btnExcluir = new JButton("Excluir");
-		btnExcluir.setMnemonic('x');
+		this.btnEditar = new JButton("Editar");
+		btnEditar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				//acao editar
+				
+				activeAction = true;
+				
+				comboBoxInstituicao.setEnabled(true);
+				
+				btnExcluir.setEnabled(false);
+				btnNova.setEnabled(false);
+				
+				btnSalvar.setEnabled(true);
+				
+				activeMnemonic = 'E';
+			}
+		});
+		this.btnEditar.setMnemonic('E');
 		
-		JButton btnSair = new JButton("Sair");
-		btnSair.setMnemonic('r');
+		this.btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				//acao excluir
+				
+				activeAction = true;
+				
+				comboBoxInstituicao.setEnabled(true);
+				
+				btnEditar.setEnabled(false);
+				btnNova.setEnabled(false);
+				
+				btnSalvar.setEnabled(true);
+				
+				activeMnemonic = 'x';
+			}
+		});
+		this.btnExcluir.setMnemonic('x');
+		
+		this.btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				//acao sair
+				
+				if(activeAction){
+					
+					comboBoxInstituicao.setEnabled(false);
+					
+					textFieldNomeInstituicao.setEnabled(false);
+					textFieldLocalizacao.setEnabled(false);
+					textFieldSilga.setEnabled(false);
+					
+					btnNova.setEnabled(true);
+					btnEditar.setEnabled(true);
+					btnExcluir.setEnabled(true);
+					
+					btnSalvar.setEnabled(false);
+					
+					activeAction = false;
+				}
+				
+				else{
+					
+					fechar();	
+				}
+				
+			}
+		});
+		this.btnSair.setMnemonic('r');
 		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
 		gl_panel_2.setHorizontalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(btnNova, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.btnNova, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnSalvar, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.btnSalvar, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.btnEditar, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnExcluir, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.btnExcluir, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnSair, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.btnSair, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(21, Short.MAX_VALUE))
 		);
 		gl_panel_2.setVerticalGroup(
@@ -121,41 +313,41 @@ public class SwingCadastrarInstituicao extends JFrame {
 				.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
 					.addContainerGap(25, Short.MAX_VALUE)
 					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnNova)
-						.addComponent(btnSalvar)
-						.addComponent(btnEditar)
-						.addComponent(btnExcluir)
-						.addComponent(btnSair))
+						.addComponent(this.btnNova)
+						.addComponent(this.btnSalvar)
+						.addComponent(this.btnEditar)
+						.addComponent(this.btnExcluir)
+						.addComponent(this.btnSair))
 					.addContainerGap())
 		);
 		panel_2.setLayout(gl_panel_2);
 		
 		JLabel lblNome = new JLabel("Nome:");
 		
-		textFieldNomeInstituicao = new JTextField();
-		textFieldNomeInstituicao.setColumns(10);
+		this.textFieldNomeInstituicao = new JTextField();
+		this.textFieldNomeInstituicao.setColumns(10);
 		
-		JLabel lblLocalizao = new JLabel("Localiza\u00E7\u00E3o:");
+		JLabel lblLocalizacao = new JLabel("Localizacao:");
 		
-		textFieldLocalizacao = new JTextField();
-		textFieldLocalizacao.setColumns(10);
+		this.textFieldLocalizacao = new JTextField();
+		this.textFieldLocalizacao.setColumns(10);
 		
 		JLabel lblSigla = new JLabel("Sigla:");
 		
-		textFieldSilga = new JTextField();
-		textFieldSilga.setColumns(10);
+		this.textFieldSilga = new JTextField();
+		this.textFieldSilga.setColumns(10);
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
 		gl_panel_1.setHorizontalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addComponent(textFieldNomeInstituicao, GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
+						.addComponent(this.textFieldNomeInstituicao, GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
 						.addComponent(lblNome)
-						.addComponent(lblLocalizao)
-						.addComponent(textFieldLocalizacao, GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
+						.addComponent(lblLocalizacao)
+						.addComponent(this.textFieldLocalizacao, GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
 						.addComponent(lblSigla)
-						.addComponent(textFieldSilga, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(this.textFieldSilga, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap())
 		);
 		gl_panel_1.setVerticalGroup(
@@ -164,39 +356,39 @@ public class SwingCadastrarInstituicao extends JFrame {
 					.addContainerGap()
 					.addComponent(lblNome)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textFieldNomeInstituicao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.textFieldNomeInstituicao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblLocalizao)
+					.addComponent(lblLocalizacao)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(textFieldLocalizacao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.textFieldLocalizacao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(lblSigla)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textFieldSilga, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.textFieldSilga, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(12, Short.MAX_VALUE))
 		);
 		panel_1.setLayout(gl_panel_1);
 		
-		JComboBox comboBoxInstituicao = new JComboBox();
+		comboBoxInstituicao = new JComboBox<String>();
 		
-		JLabel lblInstituio = new JLabel("Institui\u00E7\u00E3o:");
+		JLabel lblInstituicao = new JLabel("Instituicao:");
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(comboBoxInstituicao, 0, 488, Short.MAX_VALUE)
-						.addComponent(lblInstituio))
+						.addComponent(this.comboBoxInstituicao, 0, 488, Short.MAX_VALUE)
+						.addComponent(lblInstituicao))
 					.addContainerGap())
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblInstituio)
+					.addComponent(lblInstituicao)
 					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addComponent(comboBoxInstituicao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(this.comboBoxInstituicao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
 		panel.setLayout(gl_panel);
